f309a9b079bfbc43fcde84a26925e85c56b71092
collinchristensen <collinrchristensen@gmail.com>
2020-10-02 15:42:31 -0700

    Add color presets, hide menu during game

--- unity-ldjam-47/Assets/Scripts/ColorController.cs
+++ unity-ldjam-47/Assets/Scripts/ColorController.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ColorController : MonoBehaviour
 {
+    public Color colorA;
+    public Color colorB;
+    public Color colorC;
+    public Color colorD;
+    public Color colorE;
+
     // Start is called before the first frame update
     void Start()
     {
