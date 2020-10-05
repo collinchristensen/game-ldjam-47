af776e15bef971cedd63f2e6393927e53a140974
collinchristensen <collinrchristensen@gmail.com>
2020-10-05 01:17:56 -0700

    add enemies which chase the player

--- unity-ldjam-47/Assets/Scripts/Enemy.cs
+++ unity-ldjam-47/Assets/Scripts/Enemy.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SocialPlatforms;
 
 public class Enemy : MonoBehaviour
 {
     public float sightRadius = 5.5f;
 
     public int health;
+
+    public float moveSpeed = 2f;
 
     public Transform target;
 
@@ -57,6 +61,16 @@ public class Enemy : MonoBehaviour
         if (target != null)
         {
             Debug.Log("Player position = " + target.position.x);
+
+            float distance = Vector3.Distance(target.position, transform.position);
+
+            if (distance <= sightRadius)
+            {
+                Vector3 faceDir = target.position - transform.position;
+                faceDir.z = transform.position.z;
+
+                transform.position += faceDir * moveSpeed * Time.deltaTime;
+            }
         }
     }
 }
